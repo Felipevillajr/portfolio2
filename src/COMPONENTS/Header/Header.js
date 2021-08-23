efd8f7aefd5f079ccc021ac3a60067830d80e249
felipevillajr <felipevilla2010@live.com>
2021-08-23 13:54:28 -0400

    changed design of menu

--- src/COMPONENTS/Header/Header.js
+++ src/COMPONENTS/Header/Header.js
@@ -1,35 +1,19 @@
 import React from "react";
-import "./Header.scss";
+import "./Header.css";
 import { Link } from "react-router-dom";
 // import { makeStyles } from "@material-ui/core/styles";
 
 export default function Header() {
   return (
-    <section className="header, transparent-bg">
-      {/* <Link to="/">Home</Link>
-      <a
-        href="https://github.com/Felipevillajr/Felipevillajr"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
-      <Link to="/about">About me</Link>
-      <Link to="/portfolio">Portfolio</Link> */}
-      <h1>Felipe Villa</h1>
+    <section className="header">
+      {/* <div className="logo">Image</div> */}
       <div className="dropdown">
-        <button className="dropbtn">MENU</button>
+        <div className="homer dropbtn"></div>
+
         <div className="dropdown-content">
           <Link to="/">Home</Link>
-          <a
-            href="https://github.com/Felipevillajr/Felipevillajr"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-          <Link to="/about">About me</Link>
-          <Link to="/portfolio">Portfolio</Link>
+          <Link to="/about">About-Me</Link>
+          <Link to="/Portfolio">Portfolio</Link>
         </div>
       </div>
     </section>
